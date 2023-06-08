46a5512622710011066c1dc121e1c0f761de9cd7
root <root@PADMIN87.coaxis-asp.com>
2023-06-08 15:31:51 +0200

    bug correction

--- scripts/checkServers.php
+++ scripts/checkServers.php
@@ -17,7 +17,8 @@ function checkServer($ip)
 {
     include '../includes/db.php';
     // Récupération des serveurs
-    $stmt = $dbh->prepare("SELECT * FROM servers ORDER BY name ASC");
+    $stmt = $dbh->prepare("SELECT * FROM servers WHERE ip=:ip ORDER BY name ASC");
+    $stmt->bindValue('ip', $ip);
     $stmt->execute();
     $servers = $stmt->fetchAll();
 
@@ -50,6 +51,13 @@ function checkServer($ip)
                 $stmt->bindValue(':status', $newStatus);
                 $stmt->execute();
 
+                // Update feed
+                $tz = 'Europe/Paris';
+                $timestamp = time();
+                $currentDate = new DateTime("now", new DateTimeZone($tz)); //first argument "must" be a string
+                $currentDate->setTimestamp($timestamp); //adjust the object to correct timestamp
+                $date = $currentDate->format('Y-m-d H:i:s');
+
                 $sql = "INSERT INTO feed (name, status, date, certificate) VALUES (:name, :status, :date, :certificate)";
                 $stmt = $dbh->prepare($sql);
                 $stmt->bindValue(':name', $server['name']);
@@ -66,6 +74,13 @@ function checkServer($ip)
                 $stmt->bindValue(':name', $server['name']);
                 $stmt->bindValue(':status', $newStatus);
                 $stmt->execute();
+
+                // Update feed
+                $tz = 'Europe/Paris';
+                $timestamp = time();
+                $currentDate = new DateTime("now", new DateTimeZone($tz)); //first argument "must" be a string
+                $currentDate->setTimestamp($timestamp); //adjust the object to correct timestamp
+                $date = $currentDate->format('Y-m-d H:i:s');
 
                 $sql = "INSERT INTO feed (name, status, date, certificate) VALUES (:name, :status, :date, :certificate)";
                 $stmt = $dbh->prepare($sql);
